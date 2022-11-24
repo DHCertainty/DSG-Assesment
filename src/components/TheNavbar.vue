2c789f36e6a5b1b3983a2aca6ebca6cdf20186e9
richardsnj1 <richardsenjaya@thunderquote.com>
2022-11-24 20:42:31 +0800

    header responsiveness

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,29 +1,144 @@
 <template>
-  <header class="container-fluid front">
+  <header class="front">
     <link
       href="https://fonts.googleapis.com/css?family=Roboto"
       rel="stylesheet"
     />
     <img src="../assets/logo.png" />
     <ul>
-      <li>Date of Assessment: 22/11/2222</li>
-      <li>Client name: Abcdef</li>
+      <li class="navbar__dateAssessment">Date of Assessment: {{ dateofassessment }}</li>
+      <li class="navbar__clientName">Client name: {{ clientname }}</li>
     </ul>
     <h1>Day of Assessment</h1>
   </header>
   <br />
 </template>
 
-<style scoped>
+<script>
+export default {
+  data(){
+    return{
+      clientname: "John Doe",
+      dateofassessment: "22/11/2022",
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
 header {
   width: 100%;
   height: 10rem;
   display: flex;
   justify-content: center;
+  /* text-align: center; */
   background-color: #f6f6f6;
   position: fixed;
   overflow: hidden;
-  float: left;
+  /* float: left; */
+}
+
+@media screen and (max-width: 360px) {
+  .navbar__dateAssessment,
+  .navbar__clientName{
+    // display: none;
+    // background-color: red;
+    left: -5vw;
+    top: 5rem;
+  }
+}
+
+@media screen and (min-width: 361px) {
+  .navbar__dateAssessment,
+  .navbar__clientName{
+    // display: none;
+    // background-color: red;
+    left: 20vw;
+  }
+}
+
+@media screen and (min-width: 500px) {
+  .navbar__dateAssessment,
+  .navbar__clientName{
+    // display: none;
+    // background-color: red;
+    // display: inline-block;
+    left: 26vw;
+  }
+
+  .navbar__dateAssessment{
+    margin-right: 1rem;
+  }
+}
+
+@media screen and (min-width: 580px) {
+  .navbar__dateAssessment,
+  .navbar__clientName{
+    // display: none;
+    // background-color: red;
+    display: inline-block;
+    left: 18vw;
+  }
+
+  .navbar__dateAssessment{
+    margin-right: 1rem;
+  }
+}
+
+@media screen and (min-width: 780px) {
+  .navbar__dateAssessment,
+  .navbar__clientName{
+    // display: none;
+    // background-color: red;
+    display: inline-block;
+    left: 27vw;
+  }
+
+  .navbar__dateAssessment{
+    margin-right: 1rem;
+  }
+}
+
+@media screen and (min-width: 780px) {
+  .navbar__dateAssessment,
+  .navbar__clientName{
+    // display: none;
+    // background-color: red;
+    display: inline-block;
+    left: 27vw;
+  }
+
+  .navbar__dateAssessment{
+    margin-right: 1rem;
+  }
+}
+
+@media screen and (min-width: 950px) {
+  .navbar__dateAssessment,
+  .navbar__clientName{
+    // display: none;
+    // background-color: red;
+    display: inline-block;
+    left: 30vw;
+  }
+
+  .navbar__dateAssessment{
+    margin-right: 3rem;
+  }
+}
+
+@media screen and (min-width: 1200px) {
+  .navbar__dateAssessment,
+  .navbar__clientName{
+    // display: none;
+    // background-color: red;
+    display: inline-block;
+    left: 33.5vw;
+  }
+
+  .navbar__dateAssessment{
+    margin-right: 3rem;
+  }
 }
 
 h1 {
@@ -37,13 +152,15 @@ h1 {
 ul {
   list-style: none;
   font-size: 12px;
-  display: flex;
+  display: inline;
 }
 
 li {
   position: relative;
-  left: 70%;
-  margin-right: 6rem;
+  /* display: flex; */
+  left: 27vw;
+  top: 1rem;
+  /* margin-right: 5vw; */
 }
 
 img {
